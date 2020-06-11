225afd0a4c64497c7b896c9c1153c214cbddc99e
Midhun R Nair <midhun.rnair011@gmail.com>
2020-06-11 20:22:45 +0530

    Second Commit CP
    This commit contains
    - Dynamic URL Gen
    - Control Flow Statements in Jinja
    - Structure of blog page
    - Python Dictonary as dummy DB

--- demoPythonApp/app.py
+++ demoPythonApp/app.py
@@ -1,12 +1,31 @@
-from flask import Flask,request, render_template
-import sys
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/", methods=['GET', 'POST'])
+all_posts = [
+    {
+        'title': 'Post 1',
+        'content': 'Content of the post 1'
+    },
+    {
+        'title': 'Post 2',
+        'author': 'Midhun',
+        'content': 'Content of the post 2'
+    }
+]
+
+@app.route('/')
 def home():
-    name = ''
-    if request.method == 'POST' and 'username' in request.form:
-        name = request.form.get('username')  
-    return render_template("index.html", name = name)
+    return render_template('index.html')
+
+@app.route('/posts')
+def posts():
+    return render_template('posts.html', posts=all_posts)
+
+@app.route('/dynamic/<string:name>')
+def dynamicPage(name):
+    return 'This is an example of dynamic webpage '+ name 
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
